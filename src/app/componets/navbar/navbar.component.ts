27e61bf5b03a3ad488fb8590753353e33c0bbb91
thanhlam212 <nguyenthanhlam02122003@gmail.com>
2024-03-02 17:53:24 +0700

    navbar 1.0

---
+++ src/app/componets/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent{
+  isDropdownOpen = false;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+}
