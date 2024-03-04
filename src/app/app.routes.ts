3e2e05d3c7dbd872768ec6a616efcc153d4327b8
thanhlam212 <nguyenthanhlam02122003@gmail.com>
2024-03-04 15:43:14 +0700

    update sign in page 1.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     { path:'login', component: LoginComponent },
     { path:'homepage', component: HomepageComponent },
-    { path:'about-us', component: AboutUsComponent }
+    { path:'about-us', component: AboutUsComponent },
+    { path:'back-home', component: AppComponent}
 ];
