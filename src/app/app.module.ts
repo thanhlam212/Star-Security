246633b33774887e95c7f3654eac712ddfc13c1a
Phatdoan203 <phatdoan.1014@gmail.com>
2024-03-04 15:41:42 +0700

    update 1.0

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppRoutes,
+    BrowserModule
+  ]
+})
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path:'login', component: LoginComponent },
     { path:'homepage', component: HomepageComponent },
     { path:'about-us', component: AboutUsComponent }
 ];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutes{}
